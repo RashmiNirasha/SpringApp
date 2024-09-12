8238df65848f8aa1ac3542e609ad21f54d7f1938
L.D.R.N.GUNAWARDANA <90468021+RashmiNirasha@users.noreply.github.com>
2024-09-12 12:15:33 +0530

    Update SpringTwoApplication.java

--- src/main/java/com/example/SpringTwo/SpringTwoApplication.java
+++ src/main/java/com/example/SpringTwo/SpringTwoApplication.java
@@ -2,12 +2,20 @@ package com.example.SpringTwo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // This annotation makes this class a REST controller
 public class SpringTwoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringTwoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTwoApplication.class, args);
+    }
 
+    // This method handles GET requests to the root URL ("/")
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to SpringTwo Application!";
+    }
 }
